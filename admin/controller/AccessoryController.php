e3b8a21aee0fc868c10acd13b6b6353e824ba18d
Trugtie <nhamtrungtien2013@gmail.com>
2021-12-23 05:54:40 +0700

    Them chuc nang cap nhat lai thong tin phu kien vao csdl

--- admin/controller/AccessoryController.php
+++ admin/controller/AccessoryController.php
@@ -47,46 +47,46 @@ include "../../util/validate.php";
                 header("Location: ../view/quanlysanpham.php");
             }
             break;
-//         case "updatePhukien":
-//             $hinh = $_FILES["hinh"];
-//             $masp = $_POST['masp'];
-//             $tensp = $_POST['tensp'];
-//             $mota = $_POST['mota'];
-//             $soluong = $_POST['soluong'];
-//             $gia = $_POST['gia'];
-//             $loaisp = $_POST['loaisp'];
-//             $tinhtrang = "Còn hàng";
-//             $mahang = $_POST['mahang'];
-//             $maphukien = $_POST['maloaipk'];
+        case "updatePhukien":
+            $hinh = $_FILES["hinh"];
+            $masp = $_POST['masp'];
+            $tensp = $_POST['tensp'];
+            $mota = $_POST['mota'];
+            $soluong = $_POST['soluong'];
+            $gia = $_POST['gia'];
+            $loaisp = $_POST['loaisp'];
+            $tinhtrang = "Còn hàng";
+            $mahang = $_POST['mahang'];
+            $maphukien = $_POST['maloaipk'];
 
-//             if (!empty($hinh['name']))
-//                 $error = validate::validatePhukien($hinh, $tensp, $mota, $soluong, $gia);
-//             else
-//                 $error = validate::validatePhukienWithoutImage($tensp, $mota, $soluong, $gia);
+            if (!empty($hinh['name']))
+                $error = validate::validatePhukien($hinh, $tensp, $mota, $soluong, $gia);
+            else
+                $error = validate::validatePhukienWithoutImage($tensp, $mota, $soluong, $gia);
 
-//             if (!empty($error)) {
-//                 session_start();
-//                 $_SESSION["error"] = $error;
-//                 header("Location: ../view/editPhuKien.php?masp=$masp");
-//             } else {
-//                 if (!empty($hinh['name'])) {
-//                     $temp = $hinh["tmp_name"];
-//                     $name = $hinh["name"];
-//                     $oldPhukien =  AccessoryDAO::getPhuKien($masp, $conn);
-//                     unlink(dirname(__DIR__) . '/view' . $oldPhukien['hinh']);
-//                     move_uploaded_file($temp, dirname(__DIR__) . '/view/images/' . $name);
-//                     $imagePath = '/images/' . $name;
-//                     $phukien = new PhuKien($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $maphukien);
+            if (!empty($error)) {
+                session_start();
+                $_SESSION["error"] = $error;
+                header("Location: ../view/editPhuKien.php?masp=$masp");
+            } else {
+                if (!empty($hinh['name'])) {
+                    $temp = $hinh["tmp_name"];
+                    $name = $hinh["name"];
+                    $oldPhukien =  AccessoryDAO::getPhuKien($masp, $conn);
+                    unlink(dirname(__DIR__) . '/view' . $oldPhukien['hinh']);
+                    move_uploaded_file($temp, dirname(__DIR__) . '/view/images/' . $name);
+                    $imagePath = '/images/' . $name;
+                    $phukien = new PhuKien($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $maphukien);
                     
-//                     AccessoryDAO::updatePhuKien($phukien, $masp, $conn);
-//                     header("Location: ../view/quanlysanpham.php");
-//                 } else {
-//                     $phukien = new PhuKien($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $maphukien);
+                    AccessoryDAO::updatePhuKien($phukien, $masp, $conn);
+                    header("Location: ../view/quanlysanpham.php");
+                } else {
+                    $phukien = new PhuKien($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $maphukien);
                     
-//                     AccessoryDAO::updatePhuKienWithoutImage($phukien, $masp, $conn);
-//                     header("Location: ../view/quanlysanpham.php");
-//                 }
-//             }
-//             break;
-//         }
+                    AccessoryDAO::updatePhuKienWithoutImage($phukien, $masp, $conn);
+                    header("Location: ../view/quanlysanpham.php");
+                }
+            }
+            break;
+        // }
 }
