70a1ee2b54c32cc29c22a9a60e87713c9bce9923
Trugtie <nhamtrungtien2013@gmail.com>
2021-12-23 05:22:45 +0700

    Hoan thanh chuc nang them laptop vao csdl

--- admin/controller/productController.php
+++ admin/controller/productController.php
@@ -35,93 +35,93 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
             echo json_encode($phukiens);
             break;
     }
-}// else {
-//     $action = $_POST['action'];
-//     switch ($action) {
-//         case "addLaptop":
-//             $hinh = $_FILES["hinh"];
-//             $masp = $_POST['masp'];
-//             $tensp = $_POST['tensp'];
-//             $mota = $_POST['mota'];
-//             $soluong = $_POST['soluong'];
-//             $gia = $_POST['gia'];
-//             $loaisp = $_POST['loaisp'];
-//             $tinhtrang = "Còn hàng";
-//             $trongluong = $_POST['trongluong'];
-//             $manhinh = $_POST['manhinh'];
-//             $ocung = $_POST['ocung'];
-//             $vga = $_POST['vga'];
-//             $ram = $_POST['ram'];
-//             $cpu = $_POST['cpu'];
-//             $pin = $_POST['pin'];
-//             $mausac = $_POST['mausac'];
-//             $mahang = $_POST['mahang'];
-//             $maloaimay = $_POST['maloaimay'];
-//             $error = validate::validateLaptop($hinh, $trongluong, $manhinh, $ocung, $cpu, $ram, $tensp, $mota, $soluong, $gia, $mausac, $vga, $pin);
-//             if (!empty($error)) {
-//                 session_start();
-//                 $_SESSION["error"] = $error;
-//                 header("Location: ../view/addproduct.php");
-//             } else {
-//                 $temp = $hinh["tmp_name"];
-//                 $name = $hinh["name"];
-//                 if (!is_dir(dirname(__DIR__) . '/view/images/')) {
-//                     mkdir(dirname(__DIR__) . '/view/images/');
-//                 }
-//                 move_uploaded_file($temp, dirname(__DIR__) . '/view/images/' . $name);
-//                 $imagePath = '/images/' . $name;
-//                 $laptop = new Laptop($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $trongluong, $manhinh, $ocung, $vga, $ram, $cpu, $pin, $mausac, $maloaimay);
-//                 ProductDAO::resetAI($conn);
-//                 ProductDAO::insertLaptop($laptop, $conn);
-//                 header("Location: ../view/quanlysanpham.php");
-//             }
-//             break;
-//         case "updateLaptop":
-//             $hinh = $_FILES["hinh"];
-//             $masp = $_POST['masp'];
-//             $tensp = $_POST['tensp'];
-//             $mota = $_POST['mota'];
-//             $soluong = $_POST['soluong'];
-//             $gia = $_POST['gia'];
-//             $loaisp = $_POST['loaisp'];
-//             $tinhtrang = "Còn hàng";
-//             $trongluong = $_POST['trongluong'];
-//             $manhinh = $_POST['manhinh'];
-//             $ocung = $_POST['ocung'];
-//             $vga = $_POST['vga'];
-//             $ram = $_POST['ram'];
-//             $cpu = $_POST['cpu'];
-//             $pin = $_POST['pin'];
-//             $mausac = $_POST['mausac'];
-//             $mahang = $_POST['mahang'];
-//             $maloaimay = $_POST['maloaimay'];
+} else {
+    $action = $_POST['action'];
+    switch ($action) {
+        case "addLaptop":
+            $hinh = $_FILES["hinh"];
+            $masp = $_POST['masp'];
+            $tensp = $_POST['tensp'];
+            $mota = $_POST['mota'];
+            $soluong = $_POST['soluong'];
+            $gia = $_POST['gia'];
+            $loaisp = $_POST['loaisp'];
+            $tinhtrang = "Còn hàng";
+            $trongluong = $_POST['trongluong'];
+            $manhinh = $_POST['manhinh'];
+            $ocung = $_POST['ocung'];
+            $vga = $_POST['vga'];
+            $ram = $_POST['ram'];
+            $cpu = $_POST['cpu'];
+            $pin = $_POST['pin'];
+            $mausac = $_POST['mausac'];
+            $mahang = $_POST['mahang'];
+            $maloaimay = $_POST['maloaimay'];
+            $error = validate::validateLaptop($hinh, $trongluong, $manhinh, $ocung, $cpu, $ram, $tensp, $mota, $soluong, $gia, $mausac, $vga, $pin);
+            if (!empty($error)) {
+                session_start();
+                $_SESSION["error"] = $error;
+                header("Location: ../view/addproduct.php");
+            } else {
+                $temp = $hinh["tmp_name"];
+                $name = $hinh["name"];
+                if (!is_dir(dirname(__DIR__) . '/view/images/')) {
+                    mkdir(dirname(__DIR__) . '/view/images/');
+                }
+                move_uploaded_file($temp, dirname(__DIR__) . '/view/images/' . $name);
+                $imagePath = '/images/' . $name;
+                $laptop = new Laptop($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $trongluong, $manhinh, $ocung, $vga, $ram, $cpu, $pin, $mausac, $maloaimay);
+                ProductDAO::resetAI($conn);
+                ProductDAO::insertLaptop($laptop, $conn);
+                header("Location: ../view/quanlysanpham.php");
+            }
+            break;
+        // case "updateLaptop":
+        //     $hinh = $_FILES["hinh"];
+        //     $masp = $_POST['masp'];
+        //     $tensp = $_POST['tensp'];
+        //     $mota = $_POST['mota'];
+        //     $soluong = $_POST['soluong'];
+        //     $gia = $_POST['gia'];
+        //     $loaisp = $_POST['loaisp'];
+        //     $tinhtrang = "Còn hàng";
+        //     $trongluong = $_POST['trongluong'];
+        //     $manhinh = $_POST['manhinh'];
+        //     $ocung = $_POST['ocung'];
+        //     $vga = $_POST['vga'];
+        //     $ram = $_POST['ram'];
+        //     $cpu = $_POST['cpu'];
+        //     $pin = $_POST['pin'];
+        //     $mausac = $_POST['mausac'];
+        //     $mahang = $_POST['mahang'];
+        //     $maloaimay = $_POST['maloaimay'];
 
-//             if (!empty($hinh['name']))
-//                 $error = validate::validateLaptop($hinh, $trongluong, $manhinh, $ocung, $cpu, $ram, $tensp, $mota, $soluong, $gia, $mausac, $vga, $pin);
-//             else
-//                 $error = validate::validateLaptopWithoutImage($trongluong, $manhinh, $ocung, $cpu, $ram, $tensp, $mota, $soluong, $gia, $mausac, $vga, $pin);
+        //     if (!empty($hinh['name']))
+        //         $error = validate::validateLaptop($hinh, $trongluong, $manhinh, $ocung, $cpu, $ram, $tensp, $mota, $soluong, $gia, $mausac, $vga, $pin);
+        //     else
+        //         $error = validate::validateLaptopWithoutImage($trongluong, $manhinh, $ocung, $cpu, $ram, $tensp, $mota, $soluong, $gia, $mausac, $vga, $pin);
 
-//             if (!empty($error)) {
-//                 session_start();
-//                 $_SESSION["error"] = $error;
-//                 header("Location: ../view/editlaptop.php?masp=$masp");
-//             } else {
-//                 if (!empty($hinh['name'])) {
-//                     $temp = $hinh["tmp_name"];
-//                     $name = $hinh["name"];
-//                     $oldLaptop =  ProductDAO::getLaptop($masp, $conn);
-//                     unlink(dirname(__DIR__) . '/view' . $oldLaptop['hinh']);
-//                     move_uploaded_file($temp, dirname(__DIR__) . '/view/images/' . $name);
-//                     $imagePath = '/images/' . $name;
-//                     $laptop = new Laptop($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $trongluong, $manhinh, $ocung, $vga, $ram, $cpu, $pin, $mausac, $maloaimay);
-//                     ProductDAO::updateLaptop($laptop, $masp, $conn);
-//                     header("Location: ../view/quanlysanpham.php");
-//                 } else {
-//                     $laptop = new Laptop($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $trongluong, $manhinh, $ocung, $vga, $ram, $cpu, $pin, $mausac, $maloaimay);
-//                     ProductDAO::updateLaptopWithoutImage($laptop, $masp, $conn);
-//                     header("Location: ../view/quanlysanpham.php");
-//                 }
-//             }
-//             break;
-//     }
-// }
+        //     if (!empty($error)) {
+        //         session_start();
+        //         $_SESSION["error"] = $error;
+        //         header("Location: ../view/editlaptop.php?masp=$masp");
+        //     } else {
+        //         if (!empty($hinh['name'])) {
+        //             $temp = $hinh["tmp_name"];
+        //             $name = $hinh["name"];
+        //             $oldLaptop =  ProductDAO::getLaptop($masp, $conn);
+        //             unlink(dirname(__DIR__) . '/view' . $oldLaptop['hinh']);
+        //             move_uploaded_file($temp, dirname(__DIR__) . '/view/images/' . $name);
+        //             $imagePath = '/images/' . $name;
+        //             $laptop = new Laptop($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $trongluong, $manhinh, $ocung, $vga, $ram, $cpu, $pin, $mausac, $maloaimay);
+        //             ProductDAO::updateLaptop($laptop, $masp, $conn);
+        //             header("Location: ../view/quanlysanpham.php");
+        //         } else {
+        //             $laptop = new Laptop($masp, $tensp, $mota, $soluong, $imagePath, $gia, $loaisp, $tinhtrang, $mahang, $trongluong, $manhinh, $ocung, $vga, $ram, $cpu, $pin, $mausac, $maloaimay);
+        //             ProductDAO::updateLaptopWithoutImage($laptop, $masp, $conn);
+        //             header("Location: ../view/quanlysanpham.php");
+        //         }
+        //     }
+        //     break;
+    }
+}
